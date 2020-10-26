dc795ce34f9c60c76c6f8b830949ce7f20f57fd2
Mariana Gonçalves Rodrigues <mary.judo@gmail.com>
2020-10-26 19:46:03 -0300

    Update initRobo1.c

--- fonte/initRobo1.c
+++ fonte/initRobo1.c
@@ -1,4 +1,5 @@
 #include <BluetoothSerial.h>
+#include <string.h>
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
@@ -16,35 +17,51 @@ void setup() {
   
 }
 
-void iniciar(char comand){
-  for(char comando[x]; x<15; x++){
-    comando[x]= comand;
-  }
-  char data = SerialBT.read();
-  if(data == i){
-    for(comando[x]; x<15; x++){
-      if(comando[x] == 'f'){
-        frente();
-      }
-      if(comando[x] == 'e'){
-        esquerda();
-      }
-      if(comando[x] == 'd'){
-        direita();
-      }
+void iniciar(char *comando){
+  int tamanho = strlen(comando);   
+  for(int x=0; x<tamanho; x++){
+    if(comando[x] == 'f'){
+      frente();
     }
-  }
-  
+    if(comando[x] == 'e'){
+      esquerda();
+    }
+    if(comando[x] == 'd'){
+      direita();
+    }
+  }  
 }
 
 void loop(){
   if(SerialBT.available()){
-    for(char i[x]; x<15; x++){
-      i[x]= SerialBT.read();
-      iniciar(i[x]);
+    char *comandos = NULL;
+    char data;
+    int x = 1;
+
+    data = SerialBT.read();
+    
+    while(x<15 && data != 'i'){
+       if(data == 'f' || data == 'e' || data == 'd'){
+        Serial.println("alocando..");
+        comandos = (char*) realloc (comandos, x * sizeof(char));
+        if (comandos == NULL){
+          free(comandos);
+          exit(1);
+        }
+
+        comandos[x-1] = data;
+        Serial.println("alocado o valor:");
+        Serial.println(data);
+
+        data = SerialBT.read();
+        x++;
+       }else{
+        data = SerialBT.read();
+       }
     }
-  }
- 
+    Serial.println(data);
+    iniciar(comandos);
+  } 
 }
 
 
